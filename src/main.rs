67a0a5e68a8ff2bca7a79b36ff50c4f599500a09
simoncreek <simoncreek@tutanota.com>
2020-07-25 22:57:00 +0900

    Basic structure of progresses

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,69 @@
-use clap::clap_app;
+pub mod lib;
+
+use lib::Progress;
+use clap::{App, Arg};
+
+pub struct Arguments { }
+
+impl Arguments {
+    // These are some basic template for clap functions
+    fn get_args(&mut self) { // -> This should be changed to new value that return Arguments struct
+        let app = App::new("App name")
+            .version("1.0")
+            .about("App Description")
+            .author("Simon Creek");
+
+        // Define the name command line option
+        let name_option = Arg::with_name("name")
+            .short('n')
+            .long("name") // allow --name
+            .takes_value(true)
+            .about("Hello target")
+            .required(true);
+
+        let age_option = Arg::with_name("age")
+            .short('a')
+            .long("age") // allow --name
+            .takes_value(true)
+            .about("age of the target")
+            .required(false);
+
+        // now add in the argument we want to parse
+        let app = app.arg(name_option).arg(age_option);
+
+        // extract the matches
+        let matches = app.get_matches();
+
+        // Extract the actual name
+        let name = matches.value_of("name")
+            .expect("This can't be None, we said it was required");
+        let age = matches.value_of("age");
+
+        if let Some(value) = age {
+            println!("Hello, {} who is {}'s old!", name, value);
+        } else {
+            println!("Hello, {}!", name);
+        }
+    }
+}
 
 fn main() {
-    // Get Input from user, Parse with Clap
+    let mut new_progress = Progress::new("Test", "Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum");
 
-    let matches = clap_app!(myapp =>
-        (version: "1.0")
-        (author: "Kevin K. <kbknapp@gmail.com>")
-        (about: "Does awesome things")
-        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
-        (@arg INPUT: +required "Sets the input file to use")
-        (@arg verbose: -v --verbose "Print test information verbosely")
-        (@subcommand test =>
-         (about: "controls testing features")
-         (version: "1.3")
-         (author: "Someone E. <someone_else@other.com>")
-         (@arg debug: -d ... "Sets the level of debugging information")
-        )
-    ).get_matches();
+    println!("{}", new_progress);
+
+    new_progress.add_sub(Progress::new("SubTest", "Test Description"));
+    new_progress.add_sub(Progress::new("SubTest2", "Test Description"));
+
+    println!("{}", new_progress);
+
+    new_progress.get_subs().unwrap().get_mut("SubTest")
+        .unwrap().add_sub(Progress::new("SubsubTest", "Test Description"));
+
+    new_progress.get_subs().unwrap().get_mut("SubTest")
+        .unwrap().add_sub(Progress::new("SubsubTest2", "Test Description"));
+
+    println!("{}", new_progress);
 
     // Communicate with embedded Squlite database
     //
--- target/debug/progress_tracker.d
+++ target/debug/progress_tracker.d
@@ -1,1 +1,1 @@
-/home/simon/programming/rust/progress_tracker/target/debug/progress_tracker: /home/simon/programming/rust/progress_tracker/src/main.rs
+/home/simon/programming/rust/progress_tracker/target/debug/progress_tracker: /home/simon/programming/rust/progress_tracker/src/lib.rs /home/simon/programming/rust/progress_tracker/src/main.rs
